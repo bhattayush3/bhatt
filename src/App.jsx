c9c560b92fc9bfbc8264237e5a6994558ed69359
Ayush Bhatt <ayushbhatt963@gmail.com>
2025-06-02 14:40:52 +0530

    Fix: import paths and build success

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Home from "./components/Home";
-import NavBar from "./components/NavBar";
-import Projects from "./components/Projects";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Home from "./Components/Home";
+import NavBar from "./Components/NavBar";
+import Projects from "./Components/Projects";
 function App() {
   return (
    <div>
